c6e699a45e5c9f69c6cf7463eea5b99cadd8272c
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-29 14:56:18 +0200

    test in program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using csharp_oop_shop;
+
+Prodotto newProduct = new Prodotto("Amaca", 32);
+Console.WriteLine(newProduct.code);
+Console.WriteLine(newProduct.getExtendedName());
+newProduct.iva = 10;
+Console.WriteLine(newProduct.getPriceWIva());
+
