eccadeaac481ce52af0701b6addebf35477e5bab
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-29 14:15:56 +0200

    Creata la classe prodotto

---
+++ Prodotto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        private int codice;
+        public string name;
+        public string description;
+        public double price;
+        public int iva;
+    }
+}
